46b0abee50399ba6d0449575043e6c4678fb8f85
linshet <linshet4@gmail.com>
2020-04-28 10:28:19 +0200

    status page added to menu

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -102,6 +102,7 @@
         <?php else: ?>
         <div id="navbar" class="navbar-collapse collapse">
          <ul class="nav navbar-nav navbar-right navi" >
+			<li><a href="status.php"><i class="glyphicon glyphicon-info-sign"></i> Status</a></li>
 			<li><a href="application.php"><i class="glyphicon glyphicon-edit"></i> Bewerbung</a></li>
 			<!-- <li><a href="internalCourses.php"><i class="glyphicon glyphicon-file"></i> Fächerwahlliste</a></li> -->
 			<li><a href="test_auswahl.php"><i class="glyphicon glyphicon-check"></i> Fächerwahlliste</a></li>
