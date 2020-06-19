4b2a3869b8525c0dda85bd5c7a93afbdba2bba49
linshet <linshet4@gmail.com>
2020-06-19 10:44:24 +0200

    database name changed

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -2,7 +2,7 @@
 
 //Hier Verbindungsdaten zur Datenbank eintragen
 $db_host = 'localhost';
-$db_name = 'testdb';
+$db_name = 'ddstudportal';
 $db_user = 'root';
 $db_password = '';
 $pdo = new pdo("mysql:host=$db_host;dbname=$db_name;port=3308", $db_user, $db_password);
